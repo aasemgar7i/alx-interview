bb5d78ba38f2db19ff4e1ea6f923fea79b67e6b2
aasemgar7i <aasemgar7i@gamil.com>
2024-07-03 02:05:46 +0300

    solved

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/bin/usr/python3
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
+
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
+    return all(opened)
+
