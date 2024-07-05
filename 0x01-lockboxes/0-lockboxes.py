d587a70f553deddb15b4451abba207bb18984448
aasemgar7i <aasemgar7i@gamil.com>
2024-07-06 01:09:22 +0300

    update doc

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Parameters:
+    boxes (list of lists): A list where each element is a list of integers representing keys contained in the respective box.
+
+    Returns:
+    bool: True if all boxes can be opened, otherwise False.
+    """
     n = len(boxes)
     opened = [False] * n
     opened[0] = True
